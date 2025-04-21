95a6dd1ca7e257db32b0120621e45745af89adc7
Erwann Lagouche <21955960+AirOne01@users.noreply.github.com>
2025-04-21 16:00:18 +0200

    style(ctx_exit.c): norm

--- src/free/ctx_exit.c
+++ src/free/ctx_exit.c
@@ -6,7 +6,7 @@
 /*   By: elagouch <elagouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 17:15:04 by elagouch          #+#    #+#             */
-/*   Updated: 2025/03/18 12:30:49 by elagouch         ###   ########.fr       */
+/*   Updated: 2025/04/21 16:00:10 by elagouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
  */
 void	ctx_exit(t_ctx *ctx)
 {
-	int exit_code;
+	int	exit_code;
 
 	exit_code = ctx->exit_status;
 	ctx_clear(ctx);
